939d4090a4e1583fc1a7f1a8afa803c5eb746cef
Michael Campbell <michaelmichael323@hotmail.com>
2022-05-19 18:56:03 -0600

    getting started

---
+++ decodeString.js
@@ -0,0 +1,9 @@
+In this challenge, you’ll write a decoder.
+
+Write a function that takes in a string and returns a string. A valid argument is a number followed by a sequence of letters. Ex. 2fcjjm
+
+The number in the string represents how many characters each letter should shift. For example:
+
+>>> "1a" // "b"
+>>> "3ce" // "fh"
+>>> "2fcjjm" // "hello"
